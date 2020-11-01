d98db0db4de7d872170227870d6ef69b75a17d3c
Joost_Vos <31441672+joost-vos@users.noreply.github.com>
2020-11-01 22:39:00 +0100

    Functional Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,68 +1,53 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody,
+    CardTitle } from 'reactstrap';
 
-class DishDetail extends Component {
 
-    constructor(props) {
-        super(props)
-    }
-
-    onCommentSelect(comment) {
-        this.setState({ selectedComment: comment });
-    }
-
-    renderDish(dish) {
+    function RenderDish({dish}) {
         return (
+            <div className="col-12 col-md-5 m-1">
             <Card>
-                <CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name} />
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardBody>
-                    <CardTitle>{this.props.dish.name}</CardTitle>
-                    <CardText>{this.props.dish.description}</CardText>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
                 </CardBody>
             </Card>
-        );
-    }
-
-    renderComments(comments) {
-        // provide an empty list if no comments are available
-        const items = this.props.dish.comments || [];
-        const commentList = items.map((comment,i) => {
-            return (
-                <li key={comment.id}>
-                    <p>{comment.comment}</p>
-                    <p>-- {comment.author},
-                        &nbsp;
-                        {new Intl.DateTimeFormat('en-US', {
-                            day: '2-digit',
-                            month: 'short',
-                            year: 'numeric'
-                        }).format(new Date(Date.parse(comment.date)))}</p>
-                </li>
-            );
-        });
-
-        return (
-            <div>
-                <h4>Comments</h4>
-                <ul className="list-unstyled">
-                    {commentList}
-                </ul>
             </div>
         );
-
-
     }
 
-    render() {
-        if (this.props.dish) {
+    function RenderComments({comments}) {
+        if (comments != null)
             return (
+                <div className="col-12 col-md-5 m-1">
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                        {comments.map((comment) => {
+                            return (
+                                <li key={comment.id}>
+                                    <p>{comment.comment}</p>
+                                    <p> -- {comment.author} , {new Intl.DateTimeFormat('en-US', {day: '2-digit',month: 'short', year: 'numeric'}).format(new Date(Date.parse(comment.date)))} </p>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            );
+        else
+            return(
+                <div></div>
+            );
+    }
+
+    const DishDetail = (props) => {
+        if (props.dish != null) {
+            return (
+                <div class="container">
                 <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.props.dish)}
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderComments(this.props.dish.comments)}
-                    </div>
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments = {props.dish.comments}/>
+                </div>
                 </div>
             );
         }
@@ -72,6 +57,6 @@ class DishDetail extends Component {
             );
         }
     }
-}
+
 
 export default DishDetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,9 +5,9 @@ import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
+
     constructor(props) {
         super(props);
-
         this.state = {
             dishes: DISHES,
             selectedDish: null
@@ -26,10 +26,8 @@ class Main extends Component {
                         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
                     </div>
                 </Navbar>
-                <Menu dishes = {this.state.dishes}
-                    onClick={(dishId) => this.onDishSelect(dishId)}/>
-                <DishDetail
-                    dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                <Menu dishes = {this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
             </div>
         );
     }

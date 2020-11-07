33c9f4f8cd617b052d481c24042a4998593b43bc
Joost_Vos <31441672+joost-vos@users.noreply.github.com>
2020-11-07 11:16:08 +0100

    SPA part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -24,8 +24,7 @@ class Main extends Component {
             leaders: LEADERS
         };
     }
-
-
+    
     render() {
         const HomePage = () => {
             return(
@@ -35,12 +34,24 @@ class Main extends Component {
               />
             );
         }
+
+        const DishWithId = ({match}) => {
+            return (
+              <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+              comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+
+              />
+
+            );
+        }
+        
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
+                    <Route path="/menu/:dishId" component={DishWithId} />
                     <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" /> #can also be a 404 page
                 </Switch>
